88384df5b0b767906b96998b6af218aa2ff3c116
Filip Barańczuk <f.baranczuk@gmail.com>
2019-07-17 11:35:59 +0200

    Increase early stop patience

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ else:
     epochs=epochs,
     callbacks=[
       tensorboard,
-      EarlyStopping(patience=10, verbose=1),
+      EarlyStopping(patience=25, verbose=1),
       ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
       ModelCheckpoint('models/weights.{}.hdf5'.format(model_name), verbose=1, save_best_only=True, save_weights_only=True)
     ],
