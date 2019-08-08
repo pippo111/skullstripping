99bbc5a3b6384b15f56674ff5b8ba66081b19cd7
Filip Barańczuk <f.baranczuk@gmail.com>
2019-08-08 14:34:37 +0200

    Fix false positive / negative calculations

--- evaluate.py
+++ evaluate.py
@@ -32,19 +32,26 @@ preds_val_t = (preds_val > cfg.model['threshold']).astype(np.uint8)
 
 combined_val = y_valid + preds_val_t
 
-yellow_perc = 0
-reds_total = 0
-for image in combined_val:
-  r = np.count_nonzero(image == 1.0) # false negative (red)
-  y = np.count_nonzero(image == 2.0) # false positive (yellow)
-  g = np.count_nonzero(image == 3.0) # true positive (green)
-  mask = g + y
-  
-  yellow_perc += 0 if mask == 0 else y/mask
-  reds_total += r
+fpr_array = np.array([]) # false positive ratio (reds)
+fpr_total = np.array([]).astype('int') # false positive total pixels
+fnr_array = np.array([]) # false negative ratio (yellows)
 
-print('False positive avg % = ', '{:.0%}'.format(yellow_perc / combined_val.shape[0]))
-print('False negative total = ', reds_total)
+for image in combined_val:
+  fp = np.count_nonzero(image == 1.0) # false positive (red)
+  fn = np.count_nonzero(image == 2.0) # false negative (yellow)
+  tp = np.count_nonzero(image == 3.0) # true positive (green)
+  tn = np.count_nonzero(image == 0.0) # true negative (black)
+  
+  fpr = fp / (fp + tn) if tn != 0 or fp != 0 else 0
+  fnr = fn / (fn + tp) if tp != 0 or fn != 0 else 0
+
+  fpr_array = np.append(fpr_array, fpr)
+  fpr_total = np.append(fpr_total, fp)
+  fnr_array = np.append(fnr_array, fnr)
+
+print('False positive rate = ', '{:.0%}'.format(fpr_array.mean()))
+print('False positive total = ', fpr_total.sum())
+print('False negative rate = ', '{:.0%}'.format(fnr_array.mean()))
 
 fig_title = 'Limit={}, Loss fn: {}, Threshold: {}, Model name: {}, Loss={}, Acc={}'.format(
   cfg.dataset['limit'] or len(X_valid),
